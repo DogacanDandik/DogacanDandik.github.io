a864b022034bda89b8ee0b9c07a73714c873980e
Dogacan <dogacan.dandik@hotmail.com>
2016-10-25 16:07:24 -0400

    Reset created

---
+++ gamble/Results/resetResult.js
@@ -0,0 +1,8 @@
+/**
+ * Created by dogacandandik on 10/25/16.
+ */
+$(document).ready(function() {
+    $('#reset').on('click', function () {
+        window.location.href = 'results.html';
+    });
+});
