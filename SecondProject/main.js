3a2bf29dad5db1199ca0a70be9e960f79849a4ac
Dogacan <dogacan.dandik@hotmail.com>
2016-10-27 12:57:38 -0400

    .

--- SecondProject/main.js
+++ SecondProject/main.js
@@ -14,13 +14,16 @@ $(document).ready(function () {
     $("#btn").click(function () {
         counter++;
         $("#lbl").html(counter);
+        if(counter === 10){
+            $("#lbl2").html("Congrats you applied for 10 jobs today. It's fair!");
+        }
         if(counter === 20){
-            $("#lbl2").html("Congrats you applied for 20 jobs today");
+            $("#lbl2").css('color','orange');
+            $("#lbl2").html("Congrats you applied for 20 jobs today. Wow this is great!");
         }
         if(counter === 30){
             $("#lbl2").css('color','green');
-            $("#lbl2").html("Congrats you applied for 30 jobs today! Wow this is great!");
-
+            $("#lbl2").html("Congrats you applied for 30 jobs today! You are amazing!");
         }
     })
     
